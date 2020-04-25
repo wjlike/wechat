69d0a1c21471148cd084bc70c8c25f1dc70b7801
wjlike <wjlike@sina.cn>
2020-04-25 18:33:35 +0800

    加密

--- src/main/java/com/hsbc/wechat/config/BussinessConfig.java
+++ src/main/java/com/hsbc/wechat/config/BussinessConfig.java
@@ -1,9 +1,9 @@
 package com.hsbc.wechat.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class BussinessConfig {
 
     private static String corpid;
--- src/main/java/com/hsbc/wechat/controller/WorkWeChatController.java
+++ src/main/java/com/hsbc/wechat/controller/WorkWeChatController.java
@@ -4,6 +4,7 @@ import com.hsbc.wechat.service.WeChatContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,8 @@ public class WorkWeChatController {
 
     @Autowired
     WeChatContentService weChatContentService;
-    @GetMapping("/test")
-    public void contextLoads(long seq) {
+    @GetMapping("/test/{seq}")
+    public void contextLoads(@PathVariable("seq") long seq) {
         weChatContentService.test(seq);
     }
 }
